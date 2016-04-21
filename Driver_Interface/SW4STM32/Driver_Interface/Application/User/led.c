7289c1b2cea66db0f00b663d589b5b002da85966
thomaseliot <thomaskeliot@gmail.com>
2016-04-20 21:14:17 -0400

    begun led i2c development

--- Driver_Interface/SW4STM32/Driver_Interface/Application/User/led.c
+++ Driver_Interface/SW4STM32/Driver_Interface/Application/User/led.c
@@ -1,4 +1,11 @@
 #include "led.h"
+
+//static led_t led_bar_top[16];
+//led_bar_top[0].g_d = 4;
+//led_bar_top[0].g_p = 0;
+//led_bar_top[0].g_d = 4;
+//led_bar_top[0].g_p = 1;
+//led_bar_top[0].g_p
 void vLedUpdateTask(void * pvParameters) {
 
 	portTickType xLastWakeTime;
@@ -10,7 +17,29 @@ void vLedUpdateTask(void * pvParameters) {
 		populate memory with values of each LED address,
 		then control LEDs via I2C*/
 
-		/* 16.667ms period = 60Hz */
-		osDelayUntil( &xLastWakeTime, ( osKernelSysTickMicroSec(16667) ) );
+
+		osDelayUntil( &xLastWakeTime, ( osKernelSysTickMicroSec(period) ) );
 	}
+}
+
+void set_led(led_t sel_led, color_t led_color) {
+	//TODO: check this division works
+	int32_t alpha = get_alpha(led_color);
+	int8_t red[2] = {sel_led.r_p, (get_red(led_color) * alpha)>>8};
+	int8_t blue[2] = {sel_led.b_p, (get_blue(led_color) * alpha)>>8};
+	int8_t green[2] = {sel_led.g_p,(get_green(led_color) * alpha)>>8};
+
+
+	HAL_I2C_Master_Transmit(i2c_handler, sel_led.b_a, blue, sizeof(int8_t)*2 I2C_TIMEOUT);
+	HAL_I2C_Master_Transmit(i2c_handler, sel_led.g_a, green, sizeof(int8_t)*2, I2C_TIMEOUT);
+	HAL_I2C_Master_Transmit(i2c_handler, sel_led.r_a, red, sizeof(int8_t)*2, I2C_TIMEOUT);
+}
+
+
+void ledInitAll() {
+	//run ledInit across all leds
+}
+
+void ledInit(int LedNum) {
+
 }
--- Driver_Interface/SW4STM32/Driver_Interface/Application/User/led.h
+++ Driver_Interface/SW4STM32/Driver_Interface/Application/User/led.h
@@ -10,7 +10,44 @@
 #include "stm32f4xx_hal.h"
 #include "cmsis_os.h"
 
+/* 100Hz task freq = 10 ms period = 10,000us */
+#define period (10*10000)
+
+//BGR
+//typedef struct {
+//	int8_t b_driver;
+//	int8_t b_pin;
+//	int8_t g_driver;
+//	int8_t g_pin;
+//	int8_t r_driver;
+//	int8_t r_pin;
+//} led_t;
+//
+//#define BAR_LED_1 4,0,4,1,4,2
+//#define BAR_LED_2 4,3,4,4,4,5
+//#define BAR_LED_3 4,6,4,7,4,8
+//#define BAR_LED_4
+
+//1 byte each: alpha R G B
+typedef uint32_t color_t;
+
 typedef uint32_t portTickType;
+
+//#define I2C_TIMEOUT
+
+#define get_blue(c) (c&0xFF)
+#define get_green(c) ((c>>8)&0xFF)
+#define get_red(c) ((c>>16)&0xFF)
+#define get_alpha(c) ((c>>24)&0xFF)
+
+#define LED_DRV_1_ADR_W (96<<1)
+#define LED_DRV_2_ADR_W (97<<1)
+#define LED_DRV_3_ADR_W (98<<1)
+#define LED_DRV_4_ADR_W (99<<1)
+#define LED_DRV_5_ADR_W (100<<1)
+#define LED_DRV_6_ADR_W (101<<1)
+
+
 void vLedUpdateTask(void * pvParameters);
 
 #endif /* APPLICATION_USER_LED_H_ */
